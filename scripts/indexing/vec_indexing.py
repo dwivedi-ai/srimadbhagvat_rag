9a33ff389ca7922c8d20ee439596ea96ca71c382
Ankit Dwivedi <ankitdwivedi2524@gmail.com>
2025-04-09 15:17:38 +0530

    fixed indexing

--- scripts/indexing/vec_indexing.py
+++ scripts/indexing/vec_indexing.py
@@ -7,7 +7,7 @@ from sentence_transformers import SentenceTransformer
 from tqdm import tqdm 
 
 # --- Configuration ---
-RAW_DATA_FILE = '../../data/raw/raw_data.jsonl'
+RAW_DATA_FILE = 'data/raw/raw_data.jsonl'
 VECTOR_DB_PATH = '../../vector_db' 
 COLLECTION_NAME = "prabhupada_purports"
 EMBEDDING_MODEL_NAME = 'all-MiniLM-L6-v2'
